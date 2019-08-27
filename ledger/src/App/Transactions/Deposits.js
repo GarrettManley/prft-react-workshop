60e5ba52f826a2a7f2108241783aa9e64502b700
Garrett Manley <garrett.manley@perficient.com>
2019-08-27 15:59:19 -0500

    temp: removed deposites

--- ledger/src/App/Transactions/Deposits.js
+++ ledger/src/App/Transactions/Deposits.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Transaction from '../../Common/Components/Transaction/transaction';
 
 export default class Deposits extends React.Component {
 	title = 'Deposits';
 
 	render() {
-		return <Transaction title={this.title} />;
+		return <div>NOT IMPLEMENTED</div>;
 	}
 }
--- ledger/src/App/Transactions/index.js
+++ ledger/src/App/Transactions/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Deposits from './Deposits';
+// import Deposits from './Deposits';
 import Debits from './Debits';
 import './styles.scss';
 
 const Transactions = props => {
 	return (
 		<div className='transactions-container'>
-			<Deposits />
+			<Debits />
 			<Debits />
 		</div>
 	);
