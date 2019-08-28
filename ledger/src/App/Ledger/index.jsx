e6f5dbe7d29485fc73c660131788e660903637b4
Your NameGarrett <garrettmanley@gmail.com>
2019-08-28 08:01:15 -0500

    CREATED CONTAINER COMPONENT

--- ledger/src/App/Ledger/index.jsx
+++ ledger/src/App/Ledger/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Card from '../../Common/Components/Card';
 
 import './styles.scss';
+import Container from '../../Common/Components/Container';
 
 export default class Ledger extends React.Component {
 	render() {
 		return (
-			<div className='ledger'>
-				<Card>Test</Card>
-			</div>
+			<Container>
+				<div className='ledger'>
+					<Card>Test</Card>
+				</div>
+			</Container>
 		);
 	}
 }
